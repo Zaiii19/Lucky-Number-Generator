cd96a187e14a3a2763993aab824b29f632b5c7a5
Zaiii1407 <mendozaalzailo@gmail.com>
2023-03-16 10:22:54 +0800

    final commit

---
+++ index.js
@@ -0,0 +1,96 @@
+let x;
+let y;
+let z;
+
+document.getElementById("sixfiftyeight").onclick = function(){
+
+    n = Math.floor(Math.random() * 58) + 1;
+    u = Math.floor(Math.random() * 58) + 1;
+    m = Math.floor(Math.random() * 58) + 1;
+    b = Math.floor(Math.random() * 58) + 1;
+    e = Math.floor(Math.random() * 58) + 1;
+    r = Math.floor(Math.random() * 58) + 1;
+
+    document.getElementById("alabel").innerHTML = n;
+    document.getElementById("blabel").innerHTML = u;
+    document.getElementById("clabel").innerHTML = m;
+    document.getElementById("dlabel").innerHTML = b;
+    document.getElementById("elabel").innerHTML = e;
+    document.getElementById("flabel").innerHTML = r;
+}
+
+document.getElementById("sixfiftyfive").onclick = function(){
+
+    n = Math.floor(Math.random() * 55) + 1;
+    u = Math.floor(Math.random() * 55) + 1;
+    m = Math.floor(Math.random() * 55) + 1;
+    b = Math.floor(Math.random() * 55) + 1;
+    e = Math.floor(Math.random() * 55) + 1;
+    r = Math.floor(Math.random() * 55) + 1;
+
+    document.getElementById("glabel").innerHTML = n;
+    document.getElementById("hlabel").innerHTML = u;
+    document.getElementById("ilabel").innerHTML = m;
+    document.getElementById("jlabel").innerHTML = b;
+    document.getElementById("klabel").innerHTML = e;
+    document.getElementById("llabel").innerHTML = r;
+}
+
+document.getElementById("sixfortynine").onclick = function(){
+
+    n = Math.floor(Math.random() * 49) + 1;
+    u = Math.floor(Math.random() * 49) + 1;
+    m = Math.floor(Math.random() * 49) + 1;
+    b = Math.floor(Math.random() * 49) + 1;
+    e = Math.floor(Math.random() * 49) + 1;
+    r = Math.floor(Math.random() * 49) + 1;
+
+    document.getElementById("mlabel").innerHTML = n;
+    document.getElementById("nlabel").innerHTML = u;
+    document.getElementById("olabel").innerHTML = m;
+    document.getElementById("plabel").innerHTML = b;
+    document.getElementById("qlabel").innerHTML = e;
+    document.getElementById("rlabel").innerHTML = r;
+}
+
+document.getElementById("sixfortyfive").onclick = function(){
+
+    n = Math.floor(Math.random() * 45) + 1;
+    u = Math.floor(Math.random() * 45) + 1;
+    m = Math.floor(Math.random() * 45) + 1;
+    b = Math.floor(Math.random() * 45) + 1;
+    e = Math.floor(Math.random() * 45) + 1;
+    r = Math.floor(Math.random() * 45) + 1;
+
+    document.getElementById("slabel").innerHTML = n;
+    document.getElementById("tlabel").innerHTML = u;
+    document.getElementById("ulabel").innerHTML = m;
+    document.getElementById("vlabel").innerHTML = b;
+    document.getElementById("wlabel").innerHTML = e;
+    document.getElementById("xlabel").innerHTML = r;
+}
+
+document.getElementById("sixfortytwo").onclick = function(){
+
+    n = Math.floor(Math.random() * 42) + 1;
+    u = Math.floor(Math.random() * 42) + 1;
+    m = Math.floor(Math.random() * 42) + 1;
+    b = Math.floor(Math.random() * 42) + 1;
+    e = Math.floor(Math.random() * 42) + 1;
+    r = Math.floor(Math.random() * 42) + 1;
+
+    document.getElementById("ylabel").innerHTML = n;
+    document.getElementById("zlabel").innerHTML = u;
+    document.getElementById("aalabel").innerHTML = m;
+    document.getElementById("bblabel").innerHTML = b;
+    document.getElementById("cclabel").innerHTML = e;
+    document.getElementById("ddlabel").innerHTML = r;
+}
+
+document.getElementById("juetengButton").onclick = function(){
+    jue = Math.floor(Math.random() * 38) + 1;
+    teng = Math.floor(Math.random() * 38) + 1;
+
+    document.getElementById("eelabel").innerHTML = jue;
+    document.getElementById("fflabel").innerHTML = teng;
+}
